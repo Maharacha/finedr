89e2c3a41b6d4b607b7c4fc15e7f35a54bc0ba3a
Joakim Nyman <joakim.nyman86@gmail.com>
2019-03-13 16:21:32 +0100

    added corsheaders

--- finedr/settings.py
+++ finedr/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
